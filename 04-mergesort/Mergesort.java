54fc34e4df178dc66e90f46a01841888219bd1c6
Jake Waksbaum <jake.waksbaum@gmail.com>
2015-02-26 08:42:17 -0500

    no work

--- 04-mergesort/Mergesort.java
+++ 04-mergesort/Mergesort.java
@@ -22,7 +22,30 @@ public class Mergesort {
         return out;
     }
 
+    public List<Integer> mergeSort(List<Integer> list) {
+        if (list.size() == 0) {
+            return list;
+        } else {
+            int divider = list.size() / 2;
+            List<Integer> front = new ArrayList<Integer>();
+            List<Integer> back = new ArrayList<Integer>();
+            int i = 0;
+            for (; i < divider; i++) {
+                front.add(list.get(i));
+            }
+            for (; i < list.size(); i++) {
+                back.add(list.get(i));
+            }
+            System.out.println(front);
+            System.out.println(back);
+            return merge(mergeSort(front), mergeSort(back));
+        }
+    }
+
     public static void main(String[] args) {
+
+        Mergesort m = new Mergesort();
+
         List<Integer> a = new ArrayList<Integer>();
         a.add(1);
         a.add(5);
@@ -36,7 +59,16 @@ public class Mergesort {
         b.add(9);
         b.add(90);
 
-        System.out.println((new Mergesort()).merge(a,b));
+        List<Integer> c = new ArrayList<Integer>();
+        c.add(0);
+        c.add(90);
+        c.add(-1);
+        c.add(6);
+        c.add(9);
+
+        //System.out.println(m.merge(a,b));
+        System.out.println(c);
+        System.out.println(m.mergeSort(c));
     }
 
 }
