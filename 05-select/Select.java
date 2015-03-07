b34ecf8c2ef143bc8bb3a07b220cbbcd20a491e8
Jake Waksbaum <jake.waksbaum@gmail.com>
2015-03-06 20:54:49 -0500

    finish select, needs refactor

--- 05-select/Select.java
+++ 05-select/Select.java
@@ -3,42 +3,67 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Select {
-    public static int[] partition(int[] list) {
-        return partition(list, 0, list.length-1);
-    }
-    public static int[] partition(int[] list, int start, int end) {
-        int[] out = new int[list.length];
 
-        // COPY EVERYTHING OUTSIDE BOUNDS
-        for (int i = 0; i < start; i++) {
-            out[i] = list[i];
-        }
-        for (int i = end; i < list.length; i++) {
-            out[i] = list[i];
-        }
+    public static int select(int[] list, int rank, int low, int high) {
+        System.out.println("//////////////////////////////////////////");
+        System.out.println("low: " + low);
+        System.out.println("high: " + high);
 
-        int pivot_index = list.length / 2;
+        int pivot_index = (low + high) / 2;
         int pivot = list[pivot_index];
         System.out.println("pivot: " + pivot);
 
-        for (int i = start; i < end; i++) {
+        int[] partitioned = new int[list.length];
+
+
+        // COPY EVERYTHING OUTSIDE BOUNDS
+        for (int i = 0; i < low; i++) {
+            partitioned[i] = list[i];
+        }
+        for (int i = high+1; i < list.length; i++) {
+            partitioned[i] = list[i];
+        }
+        System.out.println("After copy: " + Arrays.toString(partitioned));
+
+        // don't want to mutate our inputs
+        int low_dest = low;
+        int high_dest = high;
+
+        for (int i = low; i <= high; i++) {
             int val = list[i];
             if (val < pivot) {
-                out[start] = val;
-                start++;
+                partitioned[low_dest] = val;
+                low_dest++;
             } else if (val > pivot) {
-                out[end] = val;
-                end--;
+                partitioned[high_dest] = val;
+                high_dest--;
             }
+            System.out.println("partitioned: " + Arrays.toString(partitioned) + ", " + val);
         }
 
-        out[start] = pivot;
+        pivot_index = low_dest;
+        partitioned[pivot_index] = pivot;
+        int pivot_rank = pivot_index + 1; // for clarity
 
-        return out;
+        System.out.println("partitioned: " + Arrays.toString(partitioned));
+        System.out.println("pivot_rank: " + pivot_rank);
+
+        if (pivot_rank > rank) {
+            return select(partitioned, rank, low, pivot_index-1);
+        } else if (pivot_rank < rank) {
+            return select(partitioned, rank, pivot_index+1, high);
+        } else {
+            return pivot;
+        }
+
+    }
+
+    public static int select(int[] list, int rank) {
+        return select(list, rank, 0, list.length-1);
     }
 
     public static void main(String[] args) {
         int[] t = {5, 2, 12, 7, 3};
-        System.out.println(Arrays.toString(Select.partition(t)));
+        System.out.println(Select.select(t, 3));
     }
 }
