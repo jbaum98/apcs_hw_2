f6b922a402ada08e2acd6ac3c05ce44e0a689c40
Jake Waksbaum <jake.waksbaum@gmail.com>
2015-04-29 20:26:51 -0400

    refactor binarytree and remove

--- 14-binarytree/BinaryTree.java
+++ 14-binarytree/BinaryTree.java
@@ -11,82 +11,94 @@ public class BinaryTree<E extends Comparable<E>> {
         root = new Node<E>(data);
     }
 
-    private Node<E>[] findNode(E data) {
-        Node<E>[] out = {null, root};
-        while (n != null) {
-            out[0] = out[1];
-            int c = out[1].getData().compareTo(data);
+    private Nodes<E> findNode(E data, boolean leaf) {
+        Nodes<E> ns = new Nodes<E>(root);
+        while (ns.current != null) {
+            int c = ns.current.getData().compareTo(data);
             if (c > 0)
-                out[1] = out[1].getLeft();
+                ns.cycleLeft();
             else if (c < 0)
-                out[1] = out[1].getRight();
-            else {
-                return out;
+                ns.cycleRight();
+            else if (!leaf) {
+                return ns;
+            } else {
+                ns.cycleRight();
             }
         }
-        out[1] = null;
-        return out;
+        ns.current = null;
+        return ns;
     }
 
-    public E search(E data) { 
-        Node<E> n =  findNode(data)[1];
-        if (n == null) return null;
-        else return n.getData();
+    private Nodes<E> findNode(E data) {
+        return findNode(data, false);
+    }
+
+    public E search(E data) {
+        Node<E> n =  findNode(data).current;
+        if (n == null) {
+            return null;
+        } else {
+            return n.getData();
+        }
     }
 
     public void insert(E data) {
         if (root == null) {
             root = new Node<E>(data);
         } else {
+            Nodes<E> ns = findNode(data, true);
             Node<E> newNode = new Node<E>(data);
-            Node<E> n = root;
-            Node<E> tmp = null;
-            while (n != null) {
-                tmp = n;
-                boolean right = n.getData().compareTo(data) < 0;
-                if (right)
-                    n = n.getRight();
-                else
-                    n = n.getLeft();
-            }
-            boolean right = tmp.getData().compareTo(data) < 0;
-            if (right)
-                tmp.setRight(newNode);
+            if (ns.right)
+                ns.parent.setRight(newNode);
             else
-                tmp.setLeft(newNode);
-
+                ns.parent.setLeft(newNode);
         }
     }
 
-    public E remove(Node<E> n) {
-        E nodeData = n.getData();
-        if (n.isLeaf()) {
+    public E remove(E data) {
+        Nodes<E> ns = findNode(data);
+        Node<E> child;
 
-        } else if (n.getRight() == null) {
-        } else if (n.getLeft()  == null) {
-        } else {
-            Node<E> l = n.getLeft();
+        if (ns.current.getLeft() == null || ns.current.getRight() == null) {
+            if (ns.current.getRight() == null) {
+                child = ns.current.getLeft();
+            } else {
+                child = ns.current.getRight();
+            }
+        } else { // full node
+            Node<E> l = ns.current.getLeft();
             while (l.getRight() != null) {
                 l = l.getRight();
             }
-            remove(l);
+            remove (l.getData());
+            child = l;
         }
+
+        if (ns.right) {
+            ns.parent.setRight(child);
+        } else {
+            ns.parent.setLeft(child);
+        }
+        return ns.current.getData();
     }
 
     private static void swap(Node original, Node replacement) {
-       original 
     }
 
     public String toString() {
-        return root.toString();
+        return ""+root;
     }
 
     public static void main(String[] args) {
         BinaryTree<Integer> b = new BinaryTree<Integer>();
         Random r = new Random();
+        int n = 0;
         for (int i = 0; i < 15; i++) {
-            b.insert(r.nextInt(50));
+            b.insert((n = r.nextInt(50)));
         }
+        System.out.println(b);
+        System.out.println("removing " + n);
+        b.remove(n);
         System.out.println(b);
     }
 }
@@ -141,5 +153,39 @@ class Node<E> {
         } else {
             return o.toString();
         }
+    }
+}
+
+class Nodes<E> {
+    public Node<E> parent;
+    public Node<E> current;
+    public boolean right;
+
+    public Nodes(Node<E> parent, Node<E> current, boolean right) {
+        this.parent  = parent;
+        this.current = current;
+        this.right   = right;
+    }
+
+    public Nodes(Node<E> b) {
+        this(null, b, false);
+    }
+
+    public void cycle(Node<E> n, boolean right) {
+        parent = current;
+        current = n;
+        this.right = right;
+    }
+
+    public void cycleLeft() {
+        cycle(current.getLeft(), false);
+    }
+
+    public void cycleRight() {
+        cycle(current.getRight(), true);
+    }
+
+    public String toString() {
+        return ""+parent;
     }
 }
